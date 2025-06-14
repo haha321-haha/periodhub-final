d4fb22fbc98cdbc1a3b584099807425d30a2815f
nodownloadgames.github.io <dutinglove@gmail.com>
2025-06-15 04:48:37 +0800

    Add files via upload

--- app/[locale]/interactive-tools/components/ConstitutionTestTool.tsx
+++ app/[locale]/interactive-tools/components/ConstitutionTestTool.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
-import { useInteractiveToolTranslations } from '../shared/hooks/useAppTranslations';
+import { useSafeTranslations } from '@/hooks/useSafeTranslations';
 import {
   Play,
   ArrowLeft,
