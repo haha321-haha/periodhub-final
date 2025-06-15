b74a4ee068a3820d7cb30e941bb2e67ca116b2e3
nodownloadgames.github.io <dutinglove@gmail.com>
2025-06-15 13:49:28 +0800

    Add files via upload

---
+++ i18n/request.ts
@@ -0,0 +1,43 @@
+import { notFound } from 'next/navigation';
+import { getRequestConfig } from 'next-intl/server';
+
+// Can be imported from a shared config
+export const locales = ['en', 'zh'] as const;
+export type Locale = (typeof locales)[number];
+
+export default getRequestConfig(async ({ requestLocale }) => {
+  // This typically corresponds to the `[locale]` segment
+  let locale = await requestLocale;
+
+  // Ensure that a valid locale is used
+  if (!locale || !locales.includes(locale as any)) {
+    locale = 'zh';
+  }
+
+  return {
+    locale,
+    messages: (await import(`../messages/${locale}.json`)).default,
+    timeZone: 'Asia/Shanghai',
+    now: new Date(),
+    formats: {
+      dateTime: {
+        short: {
+          day: 'numeric',
+          month: 'short',
+          year: 'numeric'
+        }
+      },
+      number: {
+        precise: {
+          maximumFractionDigits: 5
+        }
+      },
+      list: {
+        enumeration: {
+          style: 'long',
+          type: 'conjunction'
+        }
+      }
+    }
+  };
+});
